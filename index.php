a8692e291e57e2ff9ff29bfce44b94ec5159c061
DenverTheDino <christophe.dwwm48@gmail.com>
2023-11-28 13:00:45 +0100

    gg

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php 
     get_header();
 ?>
-    <h1><?php bloginfo( 'name' ); ?></h1>
-    <h2><?php bloginfo( 'description' ); ?></h2>
+
  
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
  
